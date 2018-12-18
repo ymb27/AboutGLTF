c9ca3632dda045a47f5766482fd8a01687f8cb71
DESKTOP-PCCSJ5A\pc20180311 <441670420@qq.com>
2018-12-18 11:04:01 +0800

    zlib解压测试

--- javascript/index.js
+++ javascript/index.js
@@ -3,32 +3,31 @@ var Splitter = require('./lib/Splitter')
 var ZlibWrapper = require('./lib/ZlibWrapper')
 var k = new Splitter()
 var z = new ZlibWrapper()
-var output = {}
-fs.readFile('./example/test.zglb', function (err, data) {
-  if (err) {
-    return console.error(err)
-  } else {
-    k.Analyse(data)
-    k.glbJson = z.decompress(k.glbJson)
-    output = new Uint8Array(k.glbHeader.length +
-      k.glbJson.length + k.glbBin.length)
-    var headerView = new DataView(k.glbHeader.buffer)
-    var jsonView = new DataView(k.glbJson.buffer)
-    var binView = new DataView(k.glbBin.buffer)
-    var outputView = new DataView(output.buffer)
-    var outputOffset = 0
-    var offset = 0
-    for (offset = 0; offset < k.glbHeader.length; ++offset) {
-      outputView.setUint8(outputOffset, headerView.getUint8(offset))
-      ++outputOffset
-    }
-    for (offset = 0; offset < k.glbJson.length; ++offset) {
-      outputView.setUint8(outputOffset, jsonView.setUint8(offset))
-      ++outputOffset
-    }
-    for (offset = 0; offset < k.glbBin.length; ++offset) {
-      outputView.setUint8(outputOffset, binView.getUint8(offset))
-      ++outputOffset
-    }
+
+var input = fs.readFileSync('./example/test.zglb')
+k.Analyse(input)
+var outputData = (function () {
+  k.glbJson = z.decompress(k.glbJson)
+  var ret = new Uint8Array(k.glbHeader.length +
+    k.glbJson.length + k.glbBin.length)
+  var outputOffset = 0
+  var offset = 0
+  for (offset = 0; offset < k.glbHeader.length; ++offset) {
+    ret[outputOffset] = k.glbHeader[offset]
+    ++outputOffset
   }
+  for (offset = 0; offset < k.glbJson.length; ++offset) {
+    ret[outputOffset] = k.glbJson[offset]
+    ++outputOffset
+  }
+  for (offset = 0; offset < k.glbBin.length; ++offset) {
+    ret[outputOffset] = k.glbBin[offset]
+    ++outputOffset
+  }
+  return ret
+})()
+var buf = Buffer.from(outputData.buffer)
+fs.writeFileSync('./example/test.glb', buf, {
+  encoding: 'buffer',
+  flag: 'w'
 })
